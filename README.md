160b6805e0ee454ec28d8f9a56b278034fa03ed7
kasra <kasraam385@gmail.com>
2026-07-29 13:04:13 +0330

    play & stop and others added

--- README.md
+++ README.md
@@ -13,8 +13,8 @@ Day 2: Metadata Parsing & Audio Engine
     Tasks:
         DONE - Install music-metadata-browser. Parse the metadata for every file added on Day 1.
         DONE - Create an AudioPlayer hook/class. Wrap the native HTML5 <audio> element.
-        Implement basic controls: Play, Pause, Next, Previous, Seek.
-        Handle the onEnded event to auto-play the next track.
+        Done - Implement basic controls: Play, Pause, Next, Previous, Seek.
+        DONE - Handle the onEnded event to auto-play the next track.
 
 Day 3: The Terminal UI (CSS & Layout)
 
--- src/components/music-player.tsx
+++ src/components/music-player.tsx
@@ -1,19 +1,67 @@
 import { useStoreMusics } from "@/state/musics-state";
-import { useRef } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import PrevButton from "./prev-button";
 import NextButton from "./next-button";
+import { Button } from "./ui/button/button";
+import { toast } from "sonner";
 
 export default function MusicPlayer() {
   const { tracks, current_index } = useStoreMusics();
-  const input_music_ref = useRef<HTMLAudioElement | null>(null);
+  const audio_ref = useRef<HTMLAudioElement | null>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
-  if (!tracks || tracks.length === 0) return null;
-  const track = tracks[current_index].file;
+  const current_track = tracks[current_index];
+
+  const audio_src = useMemo(() => {
+    if (!current_track) return null;
+    return URL.createObjectURL(current_track.file);
+  }, [current_track]);
+
+  function handle_error_play() {
+    toast.error(
+      `Browser blocked auto play. Click Play again or click next/prev button.`,
+    );
+    setIsPlaying(false);
+  }
+
+  useEffect(() => {
+    return () => {
+      if (audio_src) {
+        URL.revokeObjectURL(audio_src);
+      }
+    };
+  }, [audio_src]);
+
+  useEffect(() => {
+    if (audio_ref.current && current_track) {
+      audio_ref.current.play().catch(handle_error_play);
+    }
+  }, [current_index, current_track]);
+
+  if (!current_track) return <div>No music selected</div>;
+
+  function toggle_play() {
+    if (audio_ref.current) {
+      if (isPlaying) {
+        audio_ref.current.pause();
+      } else {
+        audio_ref.current.play().catch(handle_error_play);
+      }
+    }
+  }
+
   return (
     <div>
       <PrevButton />
       <NextButton />
-      <audio src={URL.createObjectURL(track)} controls ref={input_music_ref} />
+      <Button onClick={toggle_play}>{isPlaying ? "Pause" : "Play"}</Button>
+      <div></div>
+      <audio
+        ref={audio_ref}
+        src={audio_src || ""}
+        onPause={() => setIsPlaying(false)}
+        onPlay={() => setIsPlaying(true)}
+      />
     </div>
   );
 }
