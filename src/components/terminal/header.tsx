d3d6a881c26678902381e1b2a56d0c06a3f35b3f
kasra <kasraam385@gmail.com>
2026-08-16 11:07:47 +0330

    remove number 1

--- src/components/terminal/header.tsx
+++ src/components/terminal/header.tsx
@@ -9,9 +9,6 @@ export default function TerminalHeader() {
       <h1 className="truncate">
         {tracks[currentIndex].artist} - {tracks[currentIndex].title}
       </h1>
-      <span className="bg-card w-4 flex items-center justify-center absolute -top-3 left-2 z-10">
-        1
-      </span>
     </div>
   );
 }
